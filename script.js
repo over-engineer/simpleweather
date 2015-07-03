eeaea1752953ea192233dcbc43446f88b2729525
Jarrad Tom <contact@tomja.com>
2015-07-03 19:36:48 +1000

    Improved search function, chopped out unused code

--- script.js
+++ script.js
@@ -39,39 +39,57 @@ $(document).ready(function() {
 			$(".weather-type-img-wrapper").html('<img src="images/weather-icons/' + weatherIcon + '.svg" alt="' + weatherType + ' weather icon">');
 		});
 
-		// $(".weather-output-wrapper").append("<br><br>" + Date());
+	};
 
-	}
+	var showLocationChoices = function() {
+		$.getJSON(userCurrentWeatherURL, function(jsonData){
+			for (i in jsonData.list) {
+				$(".location-search-form").append("<button class='location-choice' id='location-choice' type='button'" + i
+					+ ">" + jsonData.list[i].name + ", " + jsonData.list[i].sys.country + "</button>");
+			}
+		});
+	};
 
-	// Update site based on location search by user
-	$(".location-search-form").submit(function(event) {
-		weatherLocation = $(".location-search-form input").val();
-		
-		userCurrentWeatherURL = "http://api.openweathermap.org/data/2.5/find?q=" + weatherLocation + "&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
-		// AJAX call to get location name
+	// This is useless, can delete
+	var changeLocation = function() {
+		// Won't need AJAX call here
 		$.getJSON(userCurrentWeatherURL, function(jsonData){
 			var city = jsonData.list[0].name;
 			var country = jsonData.list[0].sys.country;
 			weatherLocation = city + ", " + country;
+			// Forecast data loaded here so location for current and forecast data are the same
 			userForecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + weatherLocation + "&cnt=5&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
-			console.log(weatherLocation);
 		});
-		
-		updateWeatherInfo(userForecastURL,userCurrentWeatherURL);	
+	};
+
+	// Update site based on location search by user
+	$(".location-search-form").submit(function(event) {
+		// Clears if results of a previous search still present
+		$('.location-choice').remove();		
+
+		weatherLocation = $(".location-search-form input").val();		
+		userCurrentWeatherURL = "http://api.openweathermap.org/data/2.5/find?q=" + weatherLocation + "&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
+		showLocationChoices();
 
 		// Prevents default form submit behaviour (page refresh on submit)
 		event.preventDefault();
 	});
 
-	weatherLocation = "Sydney"; //TODO: remove this
+	// Using the .on() method like this allows for event listening
+	// for elements that don't exist yet. ie, .location-choice divs
+	$('.location-search-form').on('click','.location-choice',function(){
+		weatherLocation = $(this).html();
+		userCurrentWeatherURL = "http://api.openweathermap.org/data/2.5/find?q=" + weatherLocation + "&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
+		userForecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + weatherLocation + "&cnt=5&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
+		updateWeatherInfo(userForecastURL, userCurrentWeatherURL);
+		$('.location-choice').remove();
+	});
+
 	// Get weather info from api in JSON format
 	// Current day only
 	var userCurrentWeatherURL = "http://api.openweathermap.org/data/2.5/find?q=" + weatherLocation + "&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
 	// 5 day forecast
 	// find not working for forecast
 	var userForecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + weatherLocation + "&cnt=5&units=metric&APPID=3fc654da627fd43dff92f97e7b90b23f";
-
-	// Finally call the function
-	updateWeatherInfo(userForecastURL,userCurrentWeatherURL);
 
 });
